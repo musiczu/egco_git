29f3a74e6c0393e2acda46ad19f7eff1373eaf07
ravipol <ravipol.cha2545@gmail.com>
2024-04-03 15:37:03 +0700

    edit app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-print("dsadasdas")
+const express = require('express')
+const app = express()
+port = 3000
+app.get('/', (req, res) => res.send('Hello World Here are the update version'))
+
+app.listen(port, () => console.log("server start at "+port))
